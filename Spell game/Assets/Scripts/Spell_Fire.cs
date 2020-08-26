7877b6d5d8a9e7bf1e7d3f712d2be63cfe3ced7f
Noah Thompson <noah1010@me.com>
2020-08-25 19:06:55 -0700

    All Base spell stuff implimented

---
+++ Spell game/Assets/Scripts/Spell_Fire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Spell_Fire : MonoBehaviour
+{
+    #region Variables
+    public ParticleSystem[] effects;
+
+    public GameObject shot;
+    #endregion
+
+    #region Util Methods
+    public void FireSpell()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ParticleSystem effect = Instantiate(effects[0], shot.transform);
+            effect.transform.parent = GameObject.Find("====Spells====").transform;
+        }
+    }
+    #endregion
+
+    #region Main Methods
+    private void Update()
+    {
+        FireSpell();
+    }
+    #endregion
+  
+}
---
+++ Spell game/Assets/Scripts/Spell_Flight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell_Flight : MonoBehaviour
+{
+    #region Variables
+    private Vector3 fwd;
+    private float speed;
+    #endregion
+
+    private void Start()
+    {
+        fwd = transform.forward;
+        speed = Random.Range(1, 10);
+    }
+    void Update()
+    {
+        transform.position += fwd * (Time.deltaTime * speed );
+        Destroy(gameObject, 5);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+        Destroy(gameObject);
+    }
+}
